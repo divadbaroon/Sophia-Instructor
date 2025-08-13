5b7c435258929f74338be2771f2f4f342e7153b6
David <dbarron410@vt.edu>
2025-08-13 04:28:38 -0400

    init ReplayPage

---
+++ src/app/(root)/replay/[conceptId]/session/[sessionId]/page.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import React from 'react';
+
+import ReplayWorkspaceLayout from "@/components/replay/replay-work-space/ReplayWorkSpace"
+import ReplayProgressBar from "@/components/replay/replay-progress-bar/ReplayProgressBar"
+import ReplayProvider from '@/lib/providers/replay-provider/ReplayProvider';
+
+const ReplaysPage = () => {
+  return (
+    <ReplayProvider>
+      <div className="relative h-screen">
+        <div className="h-full pb-24">
+          <ReplayWorkspaceLayout />
+        </div>
+        
+        <ReplayProgressBar />
+      </div>
+    </ReplayProvider>
+  );
+};
+
+export default ReplaysPage;
